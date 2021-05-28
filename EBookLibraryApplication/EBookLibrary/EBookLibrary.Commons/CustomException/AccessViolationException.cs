b155ed3688c4d3cc5eec29dc8ba5b8768d0f7f9b
Onuh Chukwuma. M <onuhchukwuma12@gmail.com>
2021-05-28 21:02:09 +0100

    error handling

---
+++ EBookLibraryApplication/EBookLibrary/EBookLibrary.Commons/CustomException/AccessViolationException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EBookLibrary.Commons.CustomException
+{
+    public class AccessViolationException: Exception
+    {
+        public AccessViolationException(string msg) : base(msg)
+        {
+
+        }
+    }
+}
--- EBookLibraryApplication/EBookLibrary/EBookLibrary.Presentation/Startup.cs
+++ EBookLibraryApplication/EBookLibrary/EBookLibrary.Presentation/Startup.cs
@@ -1,9 +1,12 @@
+using EBookLibrary.Presentation.APIExceptionMiddleWare;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,18 +30,22 @@ namespace EBookLibrary.Presentation
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        [Obsolete]
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                loggerFactory.AddNLog();
+                
             }
             else
             {
+                app.UseStatusCodePagesWithReExecute("/Error/{0}");
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
+
+            app.UseMiddleware<ApiExceptionMiddleware>();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
