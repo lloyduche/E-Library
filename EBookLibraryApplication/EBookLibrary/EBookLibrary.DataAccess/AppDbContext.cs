41703f20ce5a88987e244ac80f7e158988a3a870
Tobiijaware <miketobicarter@gmail.com>
2021-05-28 09:39:10 -0700

    my first push

---
+++ EBookLibraryApplication/EBookLibrary/EBookLibrary.DataAccess/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using EBookLibrary.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EBookLibrary.DataAccess
+{
+    public class AppDbContext : IdentityDbContext<User>
+    {
+        public DbSet<Book> Books {get;set;}
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Rating> Ratings { get; set; }
+
+        public DbSet<Review> Reviews { get; set; }
+
+             
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
