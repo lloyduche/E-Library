620c9217dd56094bf4eb3c08ae63b0e61555d59a
Tobiijaware <miketobicarter@gmail.com>
2021-05-30 03:33:00 -0700

    forgotpass

---
+++ EBookLibraryApplication/EBookLibrary/EBookLibrary.Presentation/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EBookLibrary.Presentation.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+
+
+
+        public IActionResult PasswordReset()
+        {
+            return View();
+        }
+    }
+}
