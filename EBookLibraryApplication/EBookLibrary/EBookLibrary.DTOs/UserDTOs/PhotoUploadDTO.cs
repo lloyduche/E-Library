57d54776e9280c6e389362010a89eb7720e7e3cf
Azibataram Clement <clementazibataram@gmail.com>
2021-05-31 10:55:28 +0100

    Add feature for user photo upload

---
+++ EBookLibraryApplication/EBookLibrary/EBookLibrary.DTOs/UserDTOs/PhotoUploadDTO.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EBookLibrary.DTOs.UserDTOs
+{
+    public class PhotoUploadDTO
+    {
+        [Required]
+        public IFormFile Photo { get; set; }
+
+        [Required]
+        public string Id { get; set; }
+    }
+}
