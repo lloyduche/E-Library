71b97b0c15c1f1f2f0b392e12210c838d22b3461
John Edet <55493107+clearfountain@users.noreply.github.com>
2021-05-31 12:50:27 +0100

    user dashboard content

--- EBookLibraryApplication/EBookLibrary/EBookLibrary.Presentation/Controllers/MVControllers/DashboardController.cs
+++ EBookLibraryApplication/EBookLibrary/EBookLibrary.Presentation/Controllers/MVControllers/DashboardController.cs
@@ -5,13 +5,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using EBookLibrary.ViewModels.UserVMs;
 namespace EBookLibrary.Presentation.Controllers.MVControllers
 {
     public class DashboardController : Controller
     {
         public IActionResult Index()
         {
-            return View();
+            var userDetails = new UserDashboardViewModel
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "JD@gmail.com",
+                AvatarUrl = "~/images/dummyman.jpg"
+            };
+            return View(userDetails);
         }
     }
 }
