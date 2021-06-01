97bb8c2cf89c7cb53f45305e40f46026d8f516c7
Azibataram Clement <clementazibataram@gmail.com>
2021-06-01 20:22:35 +0100

    working on the search functionality

--- EBookLibraryApplication/EBookLibrary/EBookLibrary.Presentation/Controllers/MVControllers/BookController.cs
+++ EBookLibraryApplication/EBookLibrary/EBookLibrary.Presentation/Controllers/MVControllers/BookController.cs
@@ -1,9 +1,9 @@
 ﻿using EBookLibrary.Client.Core.Implementations;
 using EBookLibrary.ViewModels.BookVMs;
+
 using Microsoft.AspNetCore.Mvc;
+
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace EBookLibrary.Presentation.Controllers.MVControllers
@@ -11,6 +11,7 @@ namespace EBookLibrary.Presentation.Controllers.MVControllers
     public class BookController : Controller
     {
         private readonly IClientBookService _book;
+
         public BookController(IClientBookService book)
         {
             _book = book;
@@ -21,6 +22,7 @@ namespace EBookLibrary.Presentation.Controllers.MVControllers
         {
             return View();
         }
+
         [HttpPost]
         public async Task<ActionResult> Add(AddBook model)
         {
@@ -36,16 +38,15 @@ namespace EBookLibrary.Presentation.Controllers.MVControllers
         public async Task<IActionResult> UpdateBook(string id)
         {
             var data = await _book.GetBook(id);
-             return View(data);
+            return View(data);
         }
-
 
         [HttpPost]
         public async Task<IActionResult> UpdateBooks(UpdateBookViewModel model, string Id)
         {
             if (ModelState.IsValid)
             {
-                var response = await _book.UpdateBook(model,Id);
+                var response = await _book.UpdateBook(model, Id);
                 if (response.Successful is true)
                 {
                     ViewBag.Message = "Update Successful";
@@ -53,7 +54,12 @@ namespace EBookLibrary.Presentation.Controllers.MVControllers
                 }
             }
             return BadRequest();
+        }
 
+        public async Task<IActionResult> Search()
+        {
+            await Task.CompletedTask;
+            return View();
         }
     }
 }
